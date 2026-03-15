54427a85ef186b6cff5390a02f6100b42f15cebe
mapsvk <30874747+mapsvk@users.noreply.github.com>
2026-03-15 21:32:56 +0000

    update. cloud-init.

--- cloud_init/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#docker run with different parameters
-
-echo 'TESTTT!!!'
-
-docker-compose -f ./compose.yml up -d --remove-orphans --force-recreate #--rm
-
-docker-compose -f ./compose.yml logs
